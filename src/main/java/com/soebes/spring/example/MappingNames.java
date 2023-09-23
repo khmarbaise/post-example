c2e82d19178c2a9bc00d9f90e463945dae9b7460
Karl Heinz Marbaise <kama@soebes.de>
2023-09-23 11:39:35 +0200

    Improved.

--- src/main/java/com/soebes/spring/example/MappingNames.java
+++ src/main/java/com/soebes/spring/example/MappingNames.java
@@ -3,41 +3,19 @@ package com.soebes.spring.example;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.hibernate.boot.model.naming.Identifier;
-import org.hibernate.boot.model.naming.PhysicalNamingStrategyStandardImpl;
-import org.hibernate.engine.jdbc.env.spi.JdbcEnvironment;
+import org.hibernate.boot.model.naming.ImplicitJoinTableNameSource;
+import org.springframework.boot.orm.jpa.hibernate.SpringImplicitNamingStrategy;
 
 
-public class MappingNames extends PhysicalNamingStrategyStandardImpl {
+public class MappingNames extends SpringImplicitNamingStrategy {
   private static final Logger LOG = LogManager.getLogger(MappingNames.class);
 
   @Override
-  public Identifier toPhysicalCatalogName(Identifier logicalName, JdbcEnvironment context) {
-    LOG.info("toPhysicalCatalogName({})", logicalName);
-    return logicalName;
-  }
-
-  @Override
-  public Identifier toPhysicalSchemaName(Identifier logicalName, JdbcEnvironment context) {
-    LOG.info("toPhysicalSchemaName({})", logicalName);
-    return super.toPhysicalSchemaName(logicalName, context);
-  }
-
-  @Override
-  public Identifier toPhysicalTableName(Identifier logicalName, JdbcEnvironment context) {
-    LOG.info("toPhysicalTableName({})", logicalName);
-    return super.toPhysicalTableName(logicalName, context);
-  }
-
-  @Override
-  public Identifier toPhysicalSequenceName(Identifier logicalName, JdbcEnvironment context) {
-    LOG.info("toPhysicalSequenceName({})", logicalName);
-    return super.toPhysicalSequenceName(logicalName, context);
-  }
-
-  @Override
-  public Identifier toPhysicalColumnName(Identifier logicalName, JdbcEnvironment context) {
-    LOG.info("toPhysicalColumnName({})", logicalName);
-    return super.toPhysicalColumnName(logicalName, context);
+  public Identifier determineJoinTableName(ImplicitJoinTableNameSource source) {
+    LOG.info("determineJoinTableName({})", source.getOwningEntityNaming());
+    String name = source.getOwningPhysicalTableName() + "_"
+                  + source.getAssociationOwningAttributePath().getProperty();
+    return toIdentifier(name, source.getBuildingContext());
   }
 
 }
